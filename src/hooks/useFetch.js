384cbce25cf1d910003e87fa613c044cdc81b3d7
Thomas Dierickx <thomas.dierickx3@student.howest.be>
2022-03-29 09:42:27 +0200

    Done #1, Done #2, In progress #3

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setError(false);
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+        if (response.ok) {
+          setData(result);
+        } else {
+          setError(result);
+        }
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, error };
+};
+
+export default useFetch;
